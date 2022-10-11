84b5e67ebe4b668a65b12c61336be3c8159e3205
wisdom nwakaku <nwakakucc@gmail.com>
2022-10-11 15:30:38 +0100

    added somethings

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -21,8 +21,8 @@ function Home() {
               </h1>
 
               <h1 className="heading-name">
-                I'M
-                <strong className="main-name"> WISDOM CHRIS</strong>
+                I'm
+                <strong className="main-name"> Wisdom Chris</strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -39,7 +39,7 @@ function NavBar() {
     >
       <Container>
         <Navbar.Brand href="/" className="d-flex">
-          <img src={logo} className="img-fluid logo" alt="brand" />
+          <h2 className="logo">WC <span>.</span></h2>
         </Navbar.Brand>
         <Navbar.Toggle
           aria-controls="responsive-navbar-nav"
